36c51924cea7fdd7bbfb73d88df1caa4881cc008
disco <denys.hula@gmail.com>
2023-10-19 01:08:57 +0300

    renamed the scripta folder, updated the parallax logic

---
+++ Assets/Scripts/Effects/Parallax/ParallaxBackgroundService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Effects.Parallax
+{
+    public class ParallaxBackgroundService : MonoBehaviour
+    {
+        [SerializeField] private List<ParallaxBackgroundElement> parallaxBackgroundElements;
+
+        [SerializeField] private float generalMotionSpeed = 1;
+
+        void LateUpdate() => parallaxBackgroundElements.ForEach(HandleParallaxElementBehavior);
+
+        private void HandleParallaxElementBehavior(ParallaxBackgroundElement element)
+        {
+            float deltaY = generalMotionSpeed * Time.deltaTime;
+
+            // Move the layer based on parallax speed
+            Vector3 newPosition = element.ElementTransform.localPosition + Vector3.up * deltaY * element.ElementRelativeParallaxSpeed;
+            element.ElementTransform.localPosition = newPosition;
+
+            // Check if the layer has moved out of the screen
+            if (Mathf.Abs(element.ElementTransform.localPosition.y - element.ElementInitialPosition.y) >= element.ElementRepeatOffset)
+            {
+                // Move the layer back to its initial position
+                var newLocalPosition = element.ElementTransform.localPosition;
+                newLocalPosition =
+                    new Vector3(newLocalPosition.x, element.ElementInitialPosition.y, newLocalPosition.z);
+            
+                element.ElementTransform.localPosition = newLocalPosition;
+            }
+        }
+    }
+}
