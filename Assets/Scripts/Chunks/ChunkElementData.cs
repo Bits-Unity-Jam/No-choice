da6f47841a1f990afe3dac9e978987d6c025ba84
disco <denys.hula@gmail.com>
2023-10-20 02:25:02 +0300

    updated the chunk element data

--- Assets/Scripts/Chunks/ChunkElementData.cs
+++ Assets/Scripts/Chunks/ChunkElementData.cs
@@ -6,12 +6,25 @@ namespace Chunks
     [Serializable]
     public struct ChunkElementData
     {
-        [SerializeField] private string obstacleId;
+        [SerializeField] private string _obstacleId;
 
-        [SerializeField] private Vector3 localPosition;
+        [SerializeField] private Vector3 _localPosition;
 
-        [SerializeField] private Quaternion localRotation;
+        [SerializeField] private Quaternion _localRotation;
 
-        [SerializeField] private Vector3 scale;
+        [SerializeField] private Vector3 _scale;
+
+        public ChunkElementData(Vector3 scale, Quaternion localRotation, Vector3 localPosition, string obstacleId) : this()
+        {
+            _scale = scale;
+            _localRotation = localRotation;
+            _localPosition = localPosition;
+            _obstacleId = obstacleId;
+        }
+
+        public Vector3 Scale { get => _scale; set => _scale = value; }
+        public Quaternion LocalRotation { get => _localRotation; set => _localRotation = value; }
+        public Vector3 LocalPosition { get => _localPosition; set => _localPosition = value; }
+        public string ObstacleId { get => _obstacleId; set => _obstacleId = value; }
     }
 }
