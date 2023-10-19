6576d333230da14d24718b2c4e88eb4e7a341850
disco <denys.hula@gmail.com>
2023-10-20 01:49:51 +0300

    added the chunk member data struct

--- Assets/Scripts/Chunks/ChunkElementData.cs
+++ Assets/Scripts/Chunks/ChunkElementData.cs
@@ -1,9 +1,17 @@
+using System;
 using UnityEngine;
 
 namespace Chunks
 {
-    public class ChunkElementData : MonoBehaviour
+    [Serializable]
+    public struct ChunkElementData
     {
-       
+        [SerializeField] private string obstacleId;
+
+        [SerializeField] private Vector3 localPosition;
+
+        [SerializeField] private Quaternion localRotation;
+
+        [SerializeField] private Vector3 scale;
     }
 }
