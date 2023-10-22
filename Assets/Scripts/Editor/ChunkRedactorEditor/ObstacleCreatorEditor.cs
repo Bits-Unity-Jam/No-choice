b0b21c332d6fe5757007571420c8926c3215e5c4
disco <denys.hula@gmail.com>
2023-10-22 18:09:09 +0300

    started development of obstacle spawner in chunk editor

---
+++ Assets/Scripts/Editor/ChunkRedactorEditor/ObstacleCreatorEditor.cs
@@ -0,0 +1,21 @@
+using System;
+using Chunks.ChunkRedactor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.ChunkRedactorEditor
+{
+    [CustomEditor(typeof(ObstacleCreator))]
+    public class ObstacleCreatorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            int obstacleCount = Enum.GetNames(typeof(ObstacleId)).Length;
+            for (int i = 0; i < obstacleCount; i++)
+            {
+                if (GUILayout.Button($"Add {Enum.GetNames(typeof(ObstacleId))[i]}"))
+                    Debug.Log("It's alive: " + target.name);
+            }
+        }
+    }
+}
