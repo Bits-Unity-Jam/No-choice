1ae9b48ae6f40626145ceb078de5ba99100bb5e0
disco <denys.hula@gmail.com>
2023-10-23 18:03:31 +0300

    start creating the chunk spawner

---
+++ Assets/Scripts/Chunks/ChunkBehaviour/ChunkLocomotionService.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Chunks.ChunkBehaviour
+{
+    public class ChunkLocomotionService : MonoBehaviour
+    {
+        [SerializeField] private Transform[] _chunkPlaceholders;
+    }
+}
